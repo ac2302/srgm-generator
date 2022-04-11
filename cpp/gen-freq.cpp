157bb0d38e318821ee92f7e5c05e3ab62f4c1f6f
Aryan Chavan <aryan@aryansdomain.com>
2022-04-11 11:56:49 +0530

    add test compositions

--- cpp/gen-freq.cpp
+++ cpp/gen-freq.cpp
@@ -20,7 +20,24 @@ void sine_wave(float duration, float frequency, float amplitude)
     }
 }
 
+void square_wave(float duration, float frequency, float amplitude)
+{
+    uint32_t num_samples = sample_rate * duration;   // total number of samples to generate
+    float period = 1.0 / frequency;                  // time-period of each cycle in seconds
+    float samples_per_period = sample_rate * period; // the number of samples per cycle
+
+    for (uint32_t i = 0; i < num_samples; ++i)
+    {
+        constexpr int16_t max_sample = 0x7fff;
+        float angle = fmodf(i, samples_per_period) / samples_per_period; // [0..1]
+        int16_t sample = ((angle < .5) ? max_sample : -max_sample) * amplitude;
+
+        sample = htobe16(sample);
+        std::cout.write((char *)&sample, sizeof(sample)); // write out one sample
+    }
+}
+
 int main(int argc, char **argv)
 {
-    sine_wave(atof(argv[1]), atof(argv[2]), atof(argv[3]));
+    square_wave(atof(argv[1]), atof(argv[2]), atof(argv[3]));
 }
--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import subprocess
+import json
 
 
 class Sound:
@@ -30,13 +31,71 @@ def save_sounds(sounds, file="output/test.au"):
         f.write(final_output)
 
 
-sounds = [
-    Sound(0.5, 443),
-    Sound(0.5, 400),
-    Sound(0.5, 443),
-    Sound(0.5, 400),
-    Sound(0.5, 443),
-    Sound(0.5, 400)
-]
+def load_mapping(file="mappings/e-base.json"):
+    with open(file, 'r')as f:
+        return json.loads(f.read())
 
+
+def load_srgm(mapping, file="test.srgm"):
+    length = 0.5
+    gap = 0.02
+
+    specials = list(mapping['special'].values())
+
+    with open(file, 'r')as f:
+        srgm = f.read().split()
+
+    final_srgm = []
+
+    prev_note = None
+    for i, s in enumerate(srgm):
+        if s:   # gets rid of last empty line
+            # filtrering comments
+            if s[0] not in mapping['special']['comment']:
+                # print(mapping['notes'][s])
+
+                if s.startswith(mapping['special']['note-length']):
+                    length = float(
+                        s.replace(mapping['special']['note-length'], ''))
+                    continue
+
+                if s.startswith(mapping['special']['gap-length']):
+                    gap = float(
+                        s.replace(mapping['special']['gap-length'], ''))
+                    continue
+
+                if s == mapping['special']['hold']:
+                    final_srgm[-1] = Sound(final_srgm[-1].duration + length,
+                                           final_srgm[-1].frequency)
+                    continue
+
+                prev_note = s
+                # if i+1 != len(srgm):
+                #     if srgm[i+1] == s:
+
+                play_length = length
+                if i != 0:
+                    if prev_note == s:
+                        play_length -= gap
+                        final_srgm.append(Sound(gap, 0))
+
+                final_srgm.append(Sound(play_length, mapping['notes'][s]))
+
+    return final_srgm
+
+
+sounds = load_srgm(mapping=load_mapping(),
+                   file="test-compositions/vs2.srgm")
+# print(sounds)
 save_sounds(sounds)
+
+# sounds = [
+#     Sound(0.5, 443),
+#     Sound(0.5, 400),
+#     Sound(0.5, 443),
+#     Sound(0.5, 400),
+#     Sound(0.5, 443),
+#     Sound(0.5, 400)
+# ]
+
+# save_sounds(sounds)
